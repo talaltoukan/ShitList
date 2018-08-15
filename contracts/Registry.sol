8566cf08191b01872183f72868e5fb4de8280cc9
talaltoukan <talaltoukan@gmail.com>
2018-08-15 18:52:19 -0400

    Began building registry contract

---
+++ contracts/Registry.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.23;
+
+import "LinkedList.sol";
+
+contract Registry is Pausable{
+
+	mapping(string => address[]) symbolToStakers; 
+	LinkedList symbolList;
+
+
+	constructor(uint256 _gameStageLength){
+
+		owner = msg.sender;
+
+		
+	}
+
+}
