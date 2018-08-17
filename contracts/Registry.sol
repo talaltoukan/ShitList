d6d7bcde4f39c36651ff6abdabb6cdf8fab8742d
talaltoukan <talaltoukan@gmail.com>
2018-08-17 10:03:52 -0400

    Moved LinkedList to Registry for easier access

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,19 +1,91 @@
 pragma solidity ^0.4.23;
 
-import "./LinkedList.sol";
 import "./Pausable.sol";
 
 contract Registry is Pausable{
 
 	mapping(string => address[]) symbolToStakers; 
-	LinkedList symbolList;
 
-
-	constructor(uint256 _gameStageLength){
+	constructor() public{
 
 		owner = msg.sender;
-
 		
+	}
+
+
+
+
+
+	/*
+		LinkedList Implementation
+	*/
+
+	event AddedNode(bytes32 next, uint256 stake,bytes32 symbol);
+	event DeletedNode(uint256 stake, bytes32 symbol);
+
+	bytes32 public head;
+	mapping(bytes32 => Node) public nodePointers;
+	uint256 public stakePool;
+
+	struct Node{
+		bytes32 next;
+		uint256 stake;
+		bytes32 symbol;
+	}
+
+	function deleteNode(bytes32 _nodeSymbol) public returns(uint256){
+		bytes32 prevPointer = bytes32(0);
+		bytes32 curPointer = head;
+		while(curPointer != bytes32(0)){
+			bytes32 curSymbol = nodePointers[curPointer].symbol;
+			if(keccak256(abi.encodePacked(curSymbol)) == keccak256(abi.encodePacked(_nodeSymbol))){
+				stakePool = stakePool - nodePointers[curPointer].stake;
+				//If you are at head node
+				if(prevPointer == 0){
+					head = nodePointers[head].next;
+				}
+				//If you are not at head node
+				else {
+					nodePointers[prevPointer].next = nodePointers[curPointer].next;
+				}
+				return nodePointers[curPointer].stake;
+			}
+			prevPointer = curPointer;
+			curPointer = nodePointers[curPointer].next;
+		}
+		return 0;
+	}
+
+	function addNode(uint256 _newStake, bytes32 _newSymbol) public {
+		bytes32 prevPointer = bytes32(0);
+		bytes32 curPointer = head;
+		bool added = false;
+		Node memory newNode;
+		bytes32 newPointer;
+		stakePool += _newStake;
+		while(curPointer != bytes32(0)){
+			uint256 curStake = nodePointers[curPointer].stake;
+			if(curStake < _newStake){
+				newNode = Node(curPointer, _newStake, _newSymbol);
+				newPointer = keccak256(abi.encodePacked(newNode.stake, newNode.symbol, now));
+				nodePointers[newPointer] = newNode;
+				nodePointers[prevPointer].next = newPointer;
+				added = true;
+			} else {
+				prevPointer = curPointer;
+				curPointer = nodePointers[curPointer].next;
+			}
+		}
+		if(!added){
+			newNode = Node(bytes32(0), _newStake, _newSymbol);
+			newPointer = keccak256(abi.encodePacked(newNode.stake, newNode.symbol, now));
+			nodePointers[newPointer] = newNode;
+			head = newPointer;
+		}
+	}
+	
+	function getStakePool() public view returns (uint256){
+		return stakePool;
 	}
 
 }
--- test/linked-list-test.js
+++ test/linked-list-test.js
@@ -1,26 +1,19 @@
 var Web3 = require('web3');
 
-var LinkedList = artifacts.require("./LinkedList.sol");
+var Registry = artifacts.require("./Registry.sol");
 
 contract('Linked List unit tests', function([owner, donor]){
 
-	it('Init empty LinkedList', async () => {
-		ll = await LinkedList.new();
-		const totalStake = await ll.getStakePool();
-
-		assert.equal(totalStake, 0);
-	});
-
-	it('Add one element', async() => {
-		ll = await LinkedList.new();
+	it('Add one element to LinkedList', async() => {
+		ll = await Registry.new();
 		await ll.addNode(2, "EOS");
 		const totalStake = await ll.getStakePool();
 
 		assert.equal(totalStake.toNumber(), 2);
 	});
 
-	it('Add an element then delete it', async() => {
-		ll = await LinkedList.new();
+	it('Add an element to LinkedList then delete it', async() => {
+		ll = await Registry.new();
 		await ll.addNode(2, "EOS");
 		await ll.deleteNode("EOS");		
 		const totalStake = await ll.getStakePool();
